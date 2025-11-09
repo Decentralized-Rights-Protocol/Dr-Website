7709943b8737901e67f4b51000eb394b1a355973
NeonTechnoX <89855167+NeonTechno@users.noreply.github.com>
2025-11-09 16:38:20 +0000

    feat: Configure next-i18next for 8 language locales

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,12 +1,9 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['ak', 'ar', 'en', 'es', 'fr', 'h§i', 's', 'zh'],
+    localeDetection: true,
   },
-  fallbackLng: {
-    default: ['en'],
-  },
-  debug: false,
+  localePath: typeof window === 'undefined' ? require('path').resolve('./locales') : '/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
-}
-
+};
