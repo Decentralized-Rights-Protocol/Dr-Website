b1939566d43ad2ac8c619108eceddf17b70c1ed5
NeonTechnoX <89855167+NeonTechno@users.noreply.github.com>
2026-09-01 13:43:43 +0000

    fix: export token hook alias used by transfer UI

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
 "use client";
 
-export { useDeri, useDeRiBalance, useTransferDeRi, useMultipleDeRiBalances } from "./useDeri";
+export { useDeri, useDeri as useDeRi, useDeRiBalance, useTransferDeRi, useMultipleDeRiBalances } from "./useDeri";
 export { useRights, useRightsBalance, useProposals, useProposal, useCastVote, useDelegateVotingPower } from "./useRights";
 export { useWallet } from "./useWallet";
